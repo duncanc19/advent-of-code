a9ccfde213b24236d71c19ec02bb8e2ea284712f
duncanc19 <duncancarter93@gmail.com>
2020-12-03 11:59:31 +0000

    Remove extension of map and get column position using modulo

--- toboggan-trajectory.js
+++ toboggan-trajectory.js
@@ -5,46 +5,26 @@ function convertMapTextToArray(textFile) {
     return map.toString().split('\n');
 }
 
-function makeMapRequiredWidth(mapArray, movesRight, movesDown) {
-    const mapLength = mapArray.length;
-    const requiredWidth = (mapLength * movesRight)/movesDown;
-
-    let entireMapArray = []; 
-    mapArray.forEach(line => {
-        for (let i=0; line.length<=requiredWidth; i++) {
-            line += line[i];
-        }
-        entireMapArray.push(line);
-    });
-
-    return entireMapArray;
-}
-
 function countTreesHit(map, movesRight, movesDown) {
     let treesHit = 0;
-
+    const mapWidth = map[0].length;
     for (let i=0, j=0; i<map.length; i=i+movesDown, j++) {
-        if (map[i][j*movesRight] == "#") {
+        let columnPosition = j*movesRight % mapWidth;
+        if (map[i][columnPosition] == "#") {
             treesHit += 1;
         }
     }
     return treesHit;
 }
 
-function mapTobogganRouteAndCountTreesHit(mapArray, movesRight, movesDown) {
-    const entireMapArray = makeMapRequiredWidth(mapArray, movesRight, movesDown);
-    return countTreesHit(entireMapArray, movesRight, movesDown);
-}
-
 const lines = convertMapTextToArray('./map.txt');
-
 const routes = [
     [1,1], [3,1], [5,1], [7,1], [1,2]
 ]
 
 let multiplied = 1;
 routes.forEach(route => {
-    multiplied *= mapTobogganRouteAndCountTreesHit(lines, route[0], route[1]);
+    multiplied *= countTreesHit(lines, route[0], route[1]);
 })
 
 console.log(multiplied);
